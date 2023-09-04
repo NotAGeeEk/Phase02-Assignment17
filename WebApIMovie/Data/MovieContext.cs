6c407cac632abb0bfbbd90246e6b861e3f90114c
Amans <notshashank10@gmail.com>
2023-09-04 15:55:21 +0530

    first commit

---
+++ WebApIMovie/Data/MovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApIMovie.Model;
+
+namespace WebApIMovie.Data
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext (DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApIMovie.Model.Movie> Movie { get; set; } = default!;
+    }
+}
